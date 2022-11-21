42a379a389ba09fbe794247037c992023b3db6b4
panyiyong <342149467@qq.com>
2022-11-21 14:41:07 +0800

    1

---
+++ index.vue
@@ -0,0 +1,33 @@
+<!-- 首页 -->
+<template>
+  <view class="wrap"></view>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import * as api from "@/service/benefitCardUser/index.js";
+export default {
+  name: "",
+  data() {
+    return {
+      list: [],
+    };
+  },
+  computed: {
+    ...mapState({
+      store_Class: (state) => state.store_Class,
+    }),
+  },
+  methods: {
+    init() {},
+    async fetchApi() {
+      const { data } = await api.getFindByUserCardNo();
+      this.list = data.data;
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.wrap {
+}
+</style>
